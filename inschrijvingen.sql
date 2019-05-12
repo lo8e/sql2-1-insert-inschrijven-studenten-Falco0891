602833d37fe9d2870364118f8670fd107d0c134f
Falco0891 <48281977+Falco0891@users.noreply.github.com>
2019-05-12 22:32:50 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,8 @@
 USE lo8e_sql2;
-INSERT INTO klassen
-(klas_code, slb_code, cohort, opleiding_code)
+INSERT INTO studenten
+(ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
 VALUES
-('lo9e-amo1','vlt03', 2019, 'amo'), 
-('lo9e-amo2','bkr02', 2019, 'amo'), 
-('lo9e-amo3','brc01', 2019, 'amo');
+(90001,'ching','','chong','1993-07-27','2211 SG','Noordwijkerhout', 73.2, 1.86, '2018-01-01'),
+(90002,'long','','wang','1992-08-05','2152 EX','Nieuw-Vennep', 82.1, 1.87, '2018-01-01'),
+(90003,'hanki','','panki','1994-03-26','2465 AG','Rijsaterwoude', 67.0, 1.77, '2018-01-01'),
+(90004,'waytu','','low','1993-07-13','2321 HP','Leiden', 72.5, 1.85, '2018-01-01');
