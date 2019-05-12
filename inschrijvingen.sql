d16162fd522112d58eca837c2afe4d7b73cf3f60
Falco0891 <48281977+Falco0891@users.noreply.github.com>
2019-05-12 22:59:36 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,4 +1,8 @@
 USE lo8e_sql2;
-UPDATE studenten
-SET actief = false
-WHERE inschrijvings_datum <= "2017-01-01";
+SELECT studenten.voornaam, docenten.achternaam, studenten.ov_nummer, klassen_studenten.klas_code, klassen.cohort, docenten.voorletters
+FROM studenten
+INNER JOIN klassen_studenten ON studenten.ov_nummer = klassen_studenten.ov_nummer 
+INNER JOIN klassen ON klassen_studenten.klas_code = klassen.klas_code
+INNER JOIN docenten ON klassen.slb_code = docenten.docent_code
+WHERE studenten.actief = TRUE
+ORDER BY klas_code, studenten.achternaam;
